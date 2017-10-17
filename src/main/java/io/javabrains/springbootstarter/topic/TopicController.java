2632669875f8f588bfffd0426991af03ed180978
Maxime Michaud <mmichaud.pro@gmail.com>
2017-10-17 23:00:36 +0200

    Spring boot
    Session 17 et 18

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,21 +1,26 @@
 package io.javabrains.springbootstarter.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		return Arrays.asList(
-			new Topic("Spring", "Spring Framework", "Spring Framework Description"),
-			new Topic("Java", "Java Framework", "Java Framework Description"),
-			new Topic("JS", "JS Framework", "JS Framework Description")
-		);
+		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
 	}
 	
 }
